c94a77f9798613acb630909d1cdb461cf439e39a
Ryan Holmdahl <ryanlh@stanford.edu>
2018-01-31 15:32:13 -0800

    api calls in dataviewer

--- visual-debugger-client/src/App.js
+++ visual-debugger-client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <DataViewer></DataViewer>
+        <DataViewer symbol="x" />
       </div>
     );
   }
--- visual-debugger-client/src/DataViewer.js
+++ visual-debugger-client/src/DataViewer.js
@@ -4,11 +4,28 @@ class DataViewer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      'payload': 0,
-    }
+      'vizschema': 'hello world!',
+    };
   }
+
+  componentDidMount() {
+    this.requestVizSchema();
+  }
+
+  requestVizSchema(){
+    fetch('https://randomuser.me/api/')
+      .then(results => {
+        return results.json();
+      })
+      .then(({ results }) => {
+        this.setState({'vizschema' : results});
+      })
+  }
+
   render() {
-    return (<div>{this.state.payload}</div>);
+    return (
+      <div>{this.state.vizschema[0].cell}</div>
+    );
   }
 }
 
