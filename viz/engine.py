af29be4f5f8ba91303c722bc8d7c9e48f278b1ed
Ryan Holmdahl <ryanlh@stanford.edu>
2018-02-01 11:24:17 -0800

    comments for vizengine

--- viz/engine.py
+++ viz/engine.py
@@ -2,26 +2,41 @@ import json
 
 
 class VisualizationEngine:
+    """An object which statefully translates symbols corresponding to Python objects into a schema for visualization.
+
+    The VisualizationEngine is separated from the VisualDebugger so that all visualization tasks can be managed here.
+    Depending on the ultimate implementation, VisualizationEngine instances may persist between VisualDebugger
+    instances, making this decomposition useful not only for abstraction purposes.
+    """
     def __init__(self):
-        self.cache = None  # some representation of previously generated objects
+        """Initializes any internal state needed by the VisualizationEngine.
+
+        Behavior here is subject to how the VisualizationEngine is ultimately implemented and used; it may be that many
+        VisualizationEngine instances are created in a single run, or that only one is created and reused throughout
+        the run.
+        """
+        self.cache = None
 
     def generate(self, symbol_id):
-        """
-        Builds the VizSchema dictionary of a symbol with given ID for visualization purposes.
+        """Builds the VizSchema dictionary of a symbol with given ID for visualization purposes.
+
+        The dictionary is a Python object, which needs to be transformed to a string via to_json if sent over a
+        socket to a server. generate is decomposed from to_json so that the VizSchema can be used within the Python
+        environment as well.
         Args:
-            symbol_id (str): The ID of the requested symbol.
+            symbol_id (str): A unique identifier for the symbol.
 
         Returns:
-            dict: A VizSchema representation of the symbol for visualization.
+            dict: A VizSchema representation of the symbol.
         """
-        return {symbol_id: 'hi nikhil'}
         raise NotImplementedError
 
     def to_json(self, symbol_dict):
-        """
-        Converts a VizSchema dictionary of a symbol into a corresponding JSON string.
-        Notes:
-            to_json must be called from the same VisualizationEngine as was used to produce symbol_dict.
+        """Converts a VizSchema dictionary of a symbol into a corresponding JSON string.
+
+        See comment on generate for explanation of decomposition. to_json a method of VisualizationEngine so that it
+        can exploit the internal cache for object referencing, and must be called from the same VisualizationEngine
+        where the dict was created.
 
         Args:
             symbol_dict (dict): The VizSchema dictionary of a symbol.
@@ -29,5 +44,4 @@ class VisualizationEngine:
         Returns:
             str: The JSON representation of the VizSchema dictionary.
         """
-        return json.dumps(symbol_dict)
         raise NotImplementedError
