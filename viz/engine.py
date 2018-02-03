59ac7ab25e0af71b16d15934560964572f5a9181
Ryan Holmdahl <ryanlh@stanford.edu>
2018-02-02 16:58:53 -0800

    resolve engine merge conflict

--- viz/engine.py
+++ viz/engine.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 
 class VisualizationEngine:
@@ -14,30 +15,54 @@ class VisualizationEngine:
         VisualizationEngine instances are created in a single run, or that only one is created and reused throughout
         the run.
         """
-        self.id_to_obj = dict()
-        self.id_to_schema = dict()
+        self.cache = defaultdict()
 
-    def generate_namespace(self, namespace):
-        vizschema = dict()
+    def generate_namespace_schemas(self, namespace):
+        namespace_schemas = dict()
         for key, value in namespace.items():
-            self.cache[id(key)] = key
-            vizschema.append(id(key))
-        for key, value in namespace
+            print(key, value)
+            obj_id = id(value)
+            self.cache[obj_id]['obj'] = value
+            namespace_schemas[obj_id] = self.generate(obj_id, key)
+        return namespace_schemas
 
-
-    def generate(self, symbol):
+    def generate_schema(self, symbol_id, name=None):
         """Builds the VizSchema dictionary of a symbol with given ID for visualization purposes.
 
         The dictionary is a Python object, which needs to be transformed to a string via to_json if sent over a
         socket to a server. generate is decomposed from to_json so that the VizSchema can be used within the Python
         environment as well.
         Args:
-            symbol_id (str): A unique identifier for the symbol.
+            symbol_id (int): A unique identifier for the symbol.
 
         Returns:
             dict: A VizSchema representation of the symbol.
         """
-        return repr(symbol)
+        if 'schema' not in self.cache[symbol_id]:
+            self.cache[symbol_id]['schema'] = {
+                                                'type': self._type_str(symbol_id),
+                                                'str': self._viz_str(symbol_id),
+                                                'name': name,
+                                                'data': None,
+                                              }
+        return self.cache[symbol_id]['schema']
+
+    def _type_str(self, symbol_id):
+        obj = self.cache[symbol_id]['obj']
+        if type(obj) is int or type(obj) is float:
+            return 'number'
+        elif type(obj) is str:
+            return 'str'
+        elif type(obj) is dict:
+            return 'dict'
+        elif type(obj) is list:
+            return 'list'
+        elif type(obj) is set:
+            return 'set'
+        elif type()
+
+    def load_symbol_data(self, symbol_id):
+        raise NotImplementedError
 
     def to_json(self, symbol_dict):
         """Converts a VizSchema dictionary of a symbol into a corresponding JSON string.
