e03713b1684d9f35596092d11ac880a7054260b4
Nikhil Bhattasali <nikhilxb@stanford.edu>
2018-02-21 17:03:31 -0800

    Canvas actions/reducers.

--- server/client/src/actions/symboltable.js
+++ server/client/src/actions/symboltable.js
@@ -2,8 +2,15 @@ import { REF } from '../services/mockdata.js';
 
 /** Action type definitions. */
 export const SymbolTableActions = {
+<<<<<<< Updated upstream
     ENSURE_SYMBOL_DATA_LOADED:  "SYMBOLTABLE::ENSURE_SYMBOL_DATA_LOADED",
     UPDATE_NAMESPACE:    "SYMBOLTABLE::UPDATE_NAMESPACE",
+=======
+    UPDATE_NAMESPACE:    "SYMBOLTABLE::UPDATE_NAMESPACE",
+    UPDATE_SYMBOL_DATA:  "SYMBOLTABLE::UPDATE_SYMBOL_DATA",
+    FETCH_DATA_FAILED:   "SYMBOLTABLE::FETCH_DATA_FAILED",
+    PARSE_DATA_FAILED:   "SYMBOLTABLE::PARSE_DATA_FAILED",
+>>>>>>> Stashed changes
 };
 
 /** Action which updates a symbol's data with a newly-fetched object and adds shells referenced therein to the symbol
